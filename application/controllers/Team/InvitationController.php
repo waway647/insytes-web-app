67dae5d8f24f50a710413f0d5dc304f1a64f2e3b
lelouchvibritannia647 <mapula.pauljoshua@gmail.com>
2025-09-28 15:57:08 +0800

    created insytes repo

---
+++ application/controllers/Team/InvitationController.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class InvitationController extends CI_Controller {
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+}
